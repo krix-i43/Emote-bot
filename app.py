92a6117bbad73370e70f2fa9c4cd467b88669600
krix-i43 <muthumk1334@gmail.com>
2026-01-21 17:36:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -278,5 +278,5 @@ if __name__ == "__main__":
 
     # 🚀 Start Flask server
 
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
